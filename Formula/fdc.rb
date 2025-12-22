d690437af28d4db477ed20345ef6ea92a71f6250
杨琼璞 <jeffygisgreat@gmail.com>
2025-12-23 01:34:36 +0800

    🚑️ Update Formula/fdc.rb

--- Formula/fdc.rb
+++ Formula/fdc.rb
@@ -1,21 +1,26 @@
 class Fdc < Formula
-  desc "A Rust-powered CLI tool for elegantly organizing and curating files"
+  desc "A fast, elegant Rust CLI for keeping your macOS & Linux folders tidy"
   homepage "https://github.com/nicejade/fine-directory-curator"
-  url "https://github.com/nicejade/fine-directory-curator/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "6f7ffa76f19d0e91c6d083db932e0bca9ddcbe6e83d94796bba8459f7ec0b607"
-  license "MIT"
+  version "0.2.0"
 
-  head "https://github.com/nicejade/fine-directory-curator.git", branch: "main"
-
-  depends_on "rust" => :build
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/nicejade/fine-directory-curator/releases/download/v0.2.0/fdc-macos-aarch64.tar.gz"
+      sha256 "sha256:b609024dcfea0ef2b1b008275a21b324cfb5b261a3a7939a76d0552323953621"
+    else
+      url "https://github.com/nicejade/fine-directory-curator/releases/download/v0.2.0/fdc-macos-x86_64.tar.gz"
+      sha256 "sha256:7a26e9f9d3d52cb4d427998f610f964f2e035fae4e8139596c99310f2eaa6715"
+    end
+  elsif OS.linux?
+    url "https://github.com/nicejade/fine-directory-curator/releases/download/v0.2.0/fdc-linux-x86_64.tar.gz"
+    sha256 "sha256:e401504302310b759c12d6000dc986ef5ec1b57bb6b0b969708ed219ffcf9ec8"
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "fdc"
   end
 
   test do
-    # 验证二进制文件是否存在并可以运行
-    assert_match "fdc", shell_output("#{bin}/fdc --version")
+    system "#{bin}/fdc", "--version"
   end
 end
-
